807af5fbf7ae7ac1a6c7eb565bc98f990d3616a3
scwanglijun <scwanglijun@gmail.com>
2016-01-01 22:09:42 +0800

    修正代码

--- lion-admin-dao/src/main/java/com/newtouch/lion/excpetion/sample/ExceptionSample.java
+++ lion-admin-dao/src/main/java/com/newtouch/lion/excpetion/sample/ExceptionSample.java
@@ -6,7 +6,7 @@ package com.newtouch.lion.excpetion.sample;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.newtouch.lion.common.logger.util.LoggerUtil;
+import com.newtouch.lion.common.util.LoggerUtil;
 import com.newtouch.lion.exception.BusinessException;
 import com.newtouch.lion.exception.SystemException;
 
