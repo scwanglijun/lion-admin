753a9f8a348138477490c8eb80b12de82bf0a4ac
scwanglijun <scwanglijun@gmail.com>
2016-01-07 14:23:02 +0800

    更新菜单数据

--- lion-admin-service/src/main/java/com/newtouch/lion/event/impl/PushMessageEventImpl.java
+++ lion-admin-service/src/main/java/com/newtouch/lion/event/impl/PushMessageEventImpl.java
@@ -1,7 +1,6 @@
 package com.newtouch.lion.event.impl;
 
 import com.google.common.eventbus.EventBus;
-import com.newtouch.lion.common.SpringContextUtil;
 import com.newtouch.lion.event.PushMessageEvent;
 import com.newtouch.lion.model.system.Resource;
 import com.newtouch.lion.web.shiro.chain.ShiroFilterChainDefinitionManager;
@@ -10,12 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by wanglijun on 16/1/6.
  */
-@Component
+@Service
 public class PushMessageEventImpl implements InitializingBean,PushMessageEvent{
 
     private static final Logger logger= LoggerFactory.getLogger(PushMessageEvent.class);
